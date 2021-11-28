19c5d8146f94c3d1dfa21b6307814803c79d934f
Patrick Mims <patrickkevinmims@gmail.com>
2021-11-28 10:42:03 -0800

    fun with references

--- chapter08/exercises/01/src/main.cc
+++ chapter08/exercises/01/src/main.cc
@@ -2,11 +2,43 @@
 
 using namespace std;
 
-inline double square(double x) { return x * x; }
+inline void swap(int &a, int &b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void swapr(int &a, int &b)
+{
+  cout << a << "-----" << b << endl;
+}
+
+void swapp(int *a, int *b)
+{
+  cout << *a << "-----" << *b << endl;
+}
+
+void swapv(int a, int b)
+{
+  cout << a << "-----" << b << endl;
+}
 
 int main(void)
 {
-  cout << "Chapter 8: Functions" << endl;
+  int a = 10;
+  int c = 20;
+  int & b = c;
+
+  cout << a << "-----" << b << endl;
+
+  swapr(a, b);
+  /*pass the address of the variable */
+  swapp(&a, &b);
+  swapv(a, b);
+  swap(a, b);
+
+  cout << a << "after swap-----" << b << endl;
 
   return 0;
 }
